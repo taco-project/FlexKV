22fc69e7502a22999b4b4a91e7775d652a449bca
zhuofan1123 <zhuofanl@nvidia.com>
2025-08-22 15:38:14 +0800

    update unit tests for new version (#79)
    * update test cache engine
    
    * update test cache engine accel
    
    * remove some tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,3 @@ This file contains shared fixtures and setup code for all tests.
 """
 # Import fixtures from test_utils so pytest can discover them
 from test_utils import model_config, cache_config, test_config
-
-import multiprocessing as mp
-
-# Set the start method for multiprocessing to 'spawn'
-# This ensures consistent behavior across different platforms
-mp.set_start_method("spawn", force=True)
