b8b1ede6266fd28efebb6daef2f673c1413907c1
rongwei <scutizhang@tencent.com>
2025-11-04 08:49:49 +0000

    modify scripts

--- examples/trt_adaption/launch.sh
+++ examples/trt_adaption/launch.sh
@@ -1,8 +1,8 @@
 mkdir -p logs
 TIMESTAMP=$(date +%Y.%m.%d-%H:%M:%S)
 MODEL_PATH=/cfs_zhongwei/models/deepseek-ai/DeepSeek-V3.1-W4AFP8-MTP
-MAX_SEQ_LEN=49152
-MAX_NUM_TOKENS=24576
+MAX_SEQ_LEN=32764
+MAX_NUM_TOKENS=16384
 TP_SIZE=8
 EP_SIZE=$TP_SIZE
 
@@ -27,5 +27,4 @@ trtllm-serve serve $MODEL_PATH \
     --max_seq_len $MAX_SEQ_LEN \
     --max_num_tokens $MAX_NUM_TOKENS \
     --max_batch_size 16 \
-    --kv_cache_free_gpu_memory_fraction 0.75 \
     --extra_llm_api_options extra-llm-api-config-cg.yml 2>&1 | tee logs/$TIMESTAMP.log 
