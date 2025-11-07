94bab661aa6c5b916b92f7d798b03cb5d9aca2c5
rongwei <scutizhang@tencent.com>
2025-11-07 02:25:54 +0000

    modify scripts

--- examples/trt_adaption/launch.sh
+++ examples/trt_adaption/launch.sh
@@ -1,8 +1,18 @@
 mkdir -p logs
 TIMESTAMP=$(date +%Y.%m.%d-%H:%M:%S)
-MODEL_PATH=/cfs_zhongwei/models/deepseek-ai/DeepSeek-V3.1-W4AFP8-MTP
-MAX_SEQ_LEN=32764
+
+MODEL_PATH=/cfs_zhongwei/models/deepseek-ai/DeepSeek-V3.1-W4AFP8-MTP-TRTLLM
+MAX_SEQ_LEN=155648
 MAX_NUM_TOKENS=16384
+
+# MAX_SEQ_LEN=49152
+# MAX_NUM_TOKENS=24576
+
+# MODEL_PATH=/cfs_zhongwei/models/Qwen3-0.6B
+# MAX_SEQ_LEN=8192
+# MAX_NUM_TOKENS=8192
+
+BATCH_SIZE=4
 TP_SIZE=8
 EP_SIZE=$TP_SIZE
 
@@ -31,6 +41,9 @@ export FLEXKV_CONFIG_PATH="./flexkv_config.json"
 export MODEL_PATH=$MODEL_PATH
 # export CUDA_LAUNCH_BLOCKING=1
 export TENSORRT_LLM_USE_FLEXKV=1
+export TLLM_LOG_FIRST_RANK_ONLY=0
+
+bash patch_trt.sh
 
 trtllm-serve serve $MODEL_PATH \
     --host 0.0.0.0 \
@@ -40,5 +53,5 @@ trtllm-serve serve $MODEL_PATH \
     --ep_size $EP_SIZE \
     --max_seq_len $MAX_SEQ_LEN \
     --max_num_tokens $MAX_NUM_TOKENS \
-    --max_batch_size 16 \
+    --max_batch_size $BATCH_SIZE \
     --extra_llm_api_options extra-llm-api-config-cg.yml 2>&1 | tee logs/$TIMESTAMP.log 
