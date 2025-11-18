77b1d1ec9d18f5a1c5d9b9f8d6375e5f83154e82
rongwei <scutizhang@tencent.com>
2025-11-18 09:35:58 +0000

    fix rebase error

--- csrc/transfer.cuh
+++ csrc/transfer.cuh
@@ -31,6 +31,6 @@ void transfer_kv_blocks(
     int64_t cpu_kv_stride_in_bytes, int64_t cpu_layer_stride_in_bytes,
     int64_t cpu_block_stride_in_bytes, int64_t cpu_startoff_inside_chunks,
     int64_t chunk_size_in_bytes, cudaStream_t stream, int transfer_sms,
-    bool is_host_to_device, bool use_ce_transfer, bool is_mla, int gpu_block_type = 0);
+    bool is_host_to_device, bool use_ce_transfer, bool is_mla);
 
 } // namespace flexkv
