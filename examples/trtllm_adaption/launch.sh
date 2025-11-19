6bdad9cdf0474b28d8866d6e8dcdd17401cafb6c
rongwei <scutizhang@tencent.com>
2025-11-19 07:16:46 +0000

    fux scripts

--- examples/trtllm_adaption/launch.sh
+++ examples/trtllm_adaption/launch.sh
@@ -1,23 +1,16 @@
 mkdir -p logs
 TIMESTAMP=$(date +%Y.%m.%d-%H:%M:%S)
-MODEL_PATH=/cfs_zhongwei/models/deepseek-ai/DeepSeek-V3.1-W4AFP8-MTP-TRTLLM
-MAX_SEQ_LEN=155648
-MAX_NUM_TOKENS=16384
-
-
-# MODEL_PATH=/cfs_zhongwei/models/Qwen3-0.6B
-# MAX_SEQ_LEN=8192
-# MAX_NUM_TOKENS=8192
+MODEL_PATH=${1:-YOUR_MODEL_PATH}
 
 BATCH_SIZE=4
 TP_SIZE=8
 EP_SIZE=$TP_SIZE
+MAX_SEQ_LEN=155648
+MAX_NUM_TOKENS=16384
 
 export FLEXKV_CONFIG_PATH="./flexkv_config.json"
 export MODEL_PATH=$MODEL_PATH
-# export CUDA_LAUNCH_BLOCKING=1
 export TENSORRT_LLM_USE_FLEXKV=1
-export TLLM_LOG_FIRST_RANK_ONLY=0
 
 trtllm-serve serve $MODEL_PATH \
     --host 0.0.0.0 \
