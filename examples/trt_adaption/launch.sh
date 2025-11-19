e7ecf892597156e72e8bf6e89f37f7fbf86f4e9e
rongwei <scutizhang@tencent.com>
2025-11-19 03:59:30 +0000

    fix scripts

--- examples/trt_adaption/launch.sh
+++ examples/trt_adaption/launch.sh
@@ -1,12 +1,9 @@
 mkdir -p logs
 TIMESTAMP=$(date +%Y.%m.%d-%H:%M:%S)
-
 MODEL_PATH=/cfs_zhongwei/models/deepseek-ai/DeepSeek-V3.1-W4AFP8-MTP-TRTLLM
 MAX_SEQ_LEN=155648
 MAX_NUM_TOKENS=16384
 
-# MAX_SEQ_LEN=49152
-# MAX_NUM_TOKENS=24576
 
 # MODEL_PATH=/cfs_zhongwei/models/Qwen3-0.6B
 # MAX_SEQ_LEN=8192
@@ -21,8 +18,6 @@ export MODEL_PATH=$MODEL_PATH
 # export CUDA_LAUNCH_BLOCKING=1
 export TENSORRT_LLM_USE_FLEXKV=1
 export TLLM_LOG_FIRST_RANK_ONLY=0
-
-bash patch_trt.sh
 
 trtllm-serve serve $MODEL_PATH \
     --host 0.0.0.0 \
