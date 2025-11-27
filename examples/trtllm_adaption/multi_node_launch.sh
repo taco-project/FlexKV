742f58a9df401f35bdf9798ddca01aea9bab5992
rongwei <scutizhang@tencent.com>
2025-11-27 10:47:04 +0800

    fix tp16 support

--- examples/trtllm_adaption/multi_node_launch.sh
+++ examples/trtllm_adaption/multi_node_launch.sh
@@ -1,23 +1,24 @@
-mkdir -p logs
-TIMESTAMP=$(date +%Y.%m.%d-%H:%M:%S)
-MODEL_PATH=${1:-YOUR_MODEL_PATH}
-
 BATCH_SIZE=4
 TP_SIZE=16
 EP_SIZE=$TP_SIZE
-MAX_SEQ_LEN=8192
-MAX_NUM_TOKENS=8192
+MAX_SEQ_LEN=155648
+MAX_NUM_TOKENS=16384
+# MAX_SEQ_LEN=8192
+# MAX_NUM_TOKENS=8192
 HOSTFILE=/cfs_zhongwei/rongwei/scripts/flexkv/trtllm_hostfile
+MODEL_PATH=${1:-YOUR_MODEL_PATH}
 
-export FLEXKV_CONFIG_PATH="./flexkv_config.json"
+export FLEXKV_CONFIG_PATH=$(realpath "./flexkv_config.json")
 export TENSORRT_LLM_USE_FLEXKV=1
 export FLEXKV_MASTER_HOST="172.16.0.30"
 export FLEXKV_MASTER_PORTS="5556,5557,5558"
-
+export TRT_SUBPROCESS_HOST="172.16.0.30"
+export TRT_SUBPROCESS_PORTS="6667,6668,6669"
+export TLLM_LOG_FIRST_RANK_ONLY=0
 
 mpirun -np 16 \
 --hostfile $HOSTFILE \
--mca plm_rsh_args "-p 3441" \       # TODO：1. 配置 ssh 免密；2.要改成 ssh 免密的端口号
+-mca plm_rsh_args "-p 9898" \
 -mca btl tcp,self \
 -mca btl_tcp_if_include eth0 \
 -x CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
@@ -33,6 +34,13 @@ mpirun -np 16 \
 -x NCCL_SOCKET_IFNAME=eth0 \
 -x OMPI_MCA_btl=tcp,self \
 -x OMPI_MCA_btl_tcp_if_include=eth0 \
+-x FLEXKV_CONFIG_PATH \
+-x TENSORRT_LLM_USE_FLEXKV \
+-x FLEXKV_MASTER_HOST \
+-x FLEXKV_MASTER_PORTS \
+-x TLLM_LOG_FIRST_RANK_ONLY \
+-x TRT_SUBPROCESS_HOST \
+-x TRT_SUBPROCESS_PORTS \
 --allow-run-as-root \
 trtllm-llmapi-launch trtllm-serve $MODEL_PATH \
     --host 0.0.0.0 \
@@ -43,4 +51,4 @@ trtllm-llmapi-launch trtllm-serve $MODEL_PATH \
     --max_seq_len $MAX_SEQ_LEN \
     --max_num_tokens $MAX_NUM_TOKENS \
     --max_batch_size $BATCH_SIZE \
-    --extra_llm_api_options extra-llm-api-config.yml \
+    --extra_llm_api_options extra-llm-api-config.yml
