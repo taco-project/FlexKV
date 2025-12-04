831a622d27d6385a2e1a65cdb1339f5623b6d067
Yue Yu <yconquesty@gmail.com>
2025-12-04 18:02:47 +0800

    Update comments

--- csrc/gds/gds_base.h
+++ csrc/gds/gds_base.h
@@ -13,7 +13,7 @@ struct BatchReadOp {
     ssize_t* result;              // Output: bytes read or -1 on error
 };
 
-/// GPUDirect Storage base class
+/// GPUDirect Storage base class. Will be derived to support local NVMe devices and NVMe-oF targets.
 class GDSBase {
 public:
     GDSBase(int round_robin = 1);
@@ -27,35 +27,35 @@ public:
     bool is_ready() const;
 
     /**
-     * Add a new file to the manager
-     * @param filename Path to the file to add
+     * Add a new file or block device to the manager
+     * @param filename Path to the file or block device to add
      * @return true on success, false on failure
      */
     bool add_file(const char* filename);
     
     /**
-     * Remove a file from the manager
-     * @param filename Path to the file to remove
+     * Remove a file or block device from the manager
+     * @param filename Path to the file or block device to remove
      * @return true on success, false on failure
      */
     bool remove_file(const char* filename);
 
     /**
-     * Read data from storage directly to GPU memory
-     * @param filename Path to the file (will be created if not exists)
+     * Read data from (remote) storage directly to GPU memory
+     * @param filename Path to the file or block device (will be created if not exists for local I/O)
      * @param gpu_buffer Pointer to GPU memory buffer to receive data
      * @param size Number of bytes to read
-     * @param file_offset Offset in file from where to read data (default: 0)
+     * @param file_offset Offset in file or block device from where to read data (default: 0)
      * @return Number of bytes read, or -1 on error
      */
     virtual ssize_t read(const char* filename, void* gpu_buffer, size_t size, size_t file_offset = 0) = 0;
 
     /**
-     * Read data from storage directly to GPU memory asynchronously
-     * @param filename Path to the file (will be created if not exists)
+     * Read data from (remote) storage directly to GPU memory asynchronously
+     * @param filename Path to the file or block device (will be created if not exists for local I/O)
      * @param gpu_buffer Pointer to GPU memory buffer to receive data
      * @param size Number of bytes to read
-     * @param file_offset Offset in file from where to read data (default: 0)
+     * @param file_offset Offset in file or block device from where to read data (default: 0)
      * @return Number of bytes read, or -1 on error
      */
     virtual ssize_t read_async(const char* filename, void* gpu_buffer, size_t size, size_t file_offset = 0) = 0;
@@ -139,15 +139,15 @@ protected:
     bool initialize_driver();
 
     /**
-     * Open and register a single file with cuFile
-     * @param filename Path to the file
+     * Open and register a single file or block device with cuFile
+     * @param filename Path to the file or block device
      * @return true on success, false on failure
      */
     virtual bool open_file_internal(const char* filename) = 0;
 
     /**
-     * Close and cleanup a single file resource
-     * @param filename Path to the file
+     * Close and cleanup a single file or block device resource
+     * @param filename Path to the file or block device
      */
     virtual void close_file_internal(const char* filename) = 0;
 
