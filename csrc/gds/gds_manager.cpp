911b09cc701088ba95c92203674aae0b851b9943
Yue Yu <yconquesty@gmail.com>
2025-12-04 17:52:03 +0800

    Refactor GDS for accessing NVMf targets

--- csrc/gds/gds_manager.cpp
+++ csrc/gds/gds_manager.cpp
@@ -7,31 +7,11 @@
 #include <cufile.h>
 #endif
 
-#include <vector>
-#include <map>
-
 GDSManager::GDSManager(std::map<int, std::vector<std::string>>& ssd_files, 
                        int num_devices, 
                        int round_robin) 
-    : is_ready_(false), num_devices_(num_devices), 
-      round_robin_(round_robin)
-#ifdef ENABLE_GDS
-    , driver_initialized_(false), next_batch_id_(1)
-#endif
+    : GDSBase(round_robin), num_devices_(num_devices)
 {
-    if (!initialize_driver()) {
-        return;
-    }
-    
-#ifdef ENABLE_GDS
-    // Create shared CUDA stream
-    cudaError_t cuda_status = cudaStreamCreate(&shared_stream_);
-    if (cuda_status != cudaSuccess) {
-        set_error("Failed to create shared CUDA stream");
-        return;
-    }
-#endif
-    
     this->num_files_per_device_ = ssd_files[0].size();
     
     file_paths_.resize(num_devices);
@@ -52,67 +32,8 @@ GDSManager::~GDSManager() {
     cleanup();
 }
 
-bool GDSManager::is_ready() const {
-    return is_ready_;
-}
-
-const std::string& GDSManager::get_last_error() const {
-    return last_error_;
-}
-
-int GDSManager::get_num_devices() const {
-    return num_devices_;
-}
-
 int GDSManager::get_num_files_per_device() const {
     return num_files_per_device_;
-}
-
-int GDSManager::get_round_robin() const {
-    return round_robin_;
-}
-
-void GDSManager::set_error(const std::string& error) {
-    last_error_ = error;
-}
-
-bool GDSManager::initialize_driver() {
-#ifdef ENABLE_GDS
-    if (driver_initialized_) {
-        return true;
-    }
-    
-    CUfileError_t status = cuFileDriverOpen();
-    if (status.err == 0) {
-        driver_initialized_ = true;
-        return true;
-    } else {
-        set_error("Failed to initialize cuFile driver");
-        return false;
-    }
-#else
-    set_error("GDS support not compiled in (ENABLE_GDS not defined)");
-    return false;
-#endif
-}
-
-bool GDSManager::add_file(const char* filename) {
-    if (!filename) {
-        set_error("Invalid filename");
-        return false;
-    }
-    
-    return open_file_internal(filename);
-}
-
-bool GDSManager::remove_file(const char* filename) {
-    if (!filename) {
-        set_error("Invalid filename");
-        return false;
-    }
-    
-    close_file_internal(filename);
-    return true;
 }
 
 bool GDSManager::open_file_internal(const char* filename) {
@@ -247,20 +168,6 @@ size_t GDSManager::get_file_count() const {
 const std::vector<std::string>& GDSManager::get_file_paths(int device_id) const {
     return file_paths_[device_id];
 }
-
-void GDSManager::synchronize() {
-#ifdef ENABLE_GDS
-    if (is_ready_) {
-        cudaStreamSynchronize(shared_stream_);
-    }
-#endif
-}
-
-#ifdef ENABLE_GDS
-cudaStream_t GDSManager::get_stream() const {
-    return is_ready_ ? shared_stream_ : nullptr;
-}
-#endif
 
 ssize_t GDSManager::write(const char* filename, const void* gpu_data, size_t size, size_t file_offset) {
     if (!is_ready_) {
@@ -443,54 +350,6 @@ int GDSManager::batch_write(const struct BatchWriteOp* operations, int batch_siz
     batch_info_[batch_id] = batch_info;
     
     return batch_id;
-#else
-    set_error("GDS not available");
-    return -1;
-#endif
-}
-
-int GDSManager::batch_synchronize(int batch_id) {
-    if (!is_ready_) {
-        set_error("GDS Manager not ready");
-        return -1;
-    }
-    
-#ifdef ENABLE_GDS
-    // Find the batch info
-    auto it = batch_info_.find(batch_id);
-    if (it == batch_info_.end()) {
-        set_error("Invalid batch ID");
-        return -1;
-    }
-    
-    BatchInfo& batch_info = it->second;
-    CUfileBatchHandle_t batch_handle = static_cast<CUfileBatchHandle_t>(batch_info.batch_handle);
-    
-    // Wait for batch to complete
-    unsigned int num_completed = 0;
-    unsigned int nr = 0;
-    std::vector<CUfileIOEvents_t> io_events(batch_info.batch_size);
-    CUfileError_t status;
-    
-    while (num_completed != static_cast<unsigned int>(batch_info.batch_size)) {
-        memset(io_events.data(), 0, io_events.size() * sizeof(CUfileIOEvents_t));
-        nr = static_cast<unsigned int>(batch_info.batch_size);
-        status = cuFileBatchIOGetStatus(batch_handle, batch_info.batch_size, &nr, io_events.data(), NULL);	
-        if (status.err !=0) {
-            set_error("cuFileBatchIOGetStatus failed");
-            // Clean up the batch handle even if status check failed
-            cuFileBatchIODestroy(batch_handle);
-            batch_info_.erase(it);
-            return -1;
-        }
-
-        num_completed += nr;
-    } 
-    // Destroy the batch
-    cuFileBatchIODestroy(batch_handle);
-    batch_info_.erase(it);
-    
-    return 0;
 #else
     set_error("GDS not available");
     return -1;
